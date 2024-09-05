a2b4a0b9110af7d2e883e590bc5fc6447a1b23b8
Devendrakori <devendraofficial841@gmail.com>
2024-09-05 08:05:40 +0530

    commit change

--- src/com/mathematical/Armstrong.java
+++ src/com/mathematical/Armstrong.java
@@ -2,7 +2,62 @@ package com.mathematical;
 
 public class Armstrong {
 
+	public void checkArmstrong(int n)
+	{
+		int count=0;
+		int temp=n,dig;
+		while(temp>0)
+		{
+			temp/=10;
+			count++;
+		}
+		System.out.println("Number of digits in"+n+" is "+count);
+		temp=n;
+		int sum=0;
+		while(temp>0)
+		{
+			dig=temp%10;
+			temp/=10;
+			sum+=Math.pow(dig, count);
+		}
+		if(sum==n)
+		{
+			System.out.println(n+" is armstrong number.");
+		}else
+			System.out.println(n+" is not armstrong number.");
+	}
+	
+	public void checkNArmstrong(int n1,int n2)
+	{
+		
+		for(int i=n1;i<=n2;i++)
+		{
+			int count=0;
+			int temp=i,dig;
+			while(temp>0)
+			{
+				temp/=10;
+				count++;
+			}
+			temp=i;
+			int sum=0;
+			while(temp>0)
+			{
+				dig=temp%10;
+				temp/=10;
+				sum+=Math.pow(dig, count);
+			}
+			if(sum==i)
+			{
+				System.out.println(i);
+			}
+			
+		}	
+	}
+	
 	public static void main(String[] args) {
 
+		Armstrong a=new Armstrong();
+		a.checkNArmstrong(20,500);
 	}
 }
